797099e8678cda373946af1970eb3fe7e12a518a
buagre <abdigali.a20@gmail.com>
2025-01-08 21:34:52 +0500

    test

--- bot.py
+++ bot.py
@@ -1,1 +1,7 @@
-print("hello world")
+from telebot import telebot
+
+
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, 'Hello! This is a simple bot that responds to /start command.')
